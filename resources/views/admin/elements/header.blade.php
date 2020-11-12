3e096390267a10d7bb34b20e357a7cd7f830d8a6
baglan-s <baglansariev@mail.ru>
2020-11-12 18:45:31 +0600

    test logout

--- resources/views/admin/elements/header.blade.php
+++ resources/views/admin/elements/header.blade.php
@@ -94,12 +94,13 @@
                 <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
                     <div class="nav-user-info">
                         <h5 class="mb-0 text-white nav-user-name">
-                            John Abraham</h5>
+                            {{ Auth()->user()->name ?? '' }}</h5>
                         <span class="status"></span><span class="ml-2">Available</span>
                     </div>
                     <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
                     <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a>
                     <form action="{{ route('logout') }}" method="post">
+                        @csrf
                         <button type="submit" class="dropdown-item"><i class="fas fa-power-off mr-2"></i>Выход</button>
                     </form>
                 </div>
