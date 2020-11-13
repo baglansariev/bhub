6f9e57abfc7f1e368398f9dcd833418c83923d97
Arman85 <advanced315@gmail.com>
2020-11-13 14:03:36 +0600

    Send Password Reset Link fix frontend

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card card-send-password-reset-link">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
